bc000a2d495b228d5ea40961f3416a06a2657c7d
Saeel <saeeldhatrak007@gmail.com>
2024-09-04 17:57:24 +0530

    My Initial Commit

---
+++ src/main/java/electoral_bond/member_portal/MemberPortalApplication.java
@@ -0,0 +1,13 @@
+package electoral_bond.member_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/electoral_bond/member_portal/MemberPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package electoral_bond.member_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
